fa3a5f24e14d6c9a474dc2ebb97a0c5b64cc2e3d
ivaniuss <ivan.emi94@gmail.com>
2024-10-02 00:23:06 -0500

    done

---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import { Experience } from "./components/Experience";
+
+export default function Home() {
+  return (
+    <main className="h-screen w-screen">
+      <Experience />
+    </main>
+  );
+}
